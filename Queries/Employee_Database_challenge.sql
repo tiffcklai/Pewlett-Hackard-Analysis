c71685d7ee88d1fd4b2d2e1c3505b92589b8e39a
tiffcklai <76405486+tiffcklai@users.noreply.github.com>
2021-02-14 13:05:32 -0500

    Challenge Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,5 +1,5 @@
 -- Challenge code 
--- Deliverable 1 
+-- Deliverable 1: The number of retiring employees by title
 
 SELECT e.emp_no,
 	e.first_name,
@@ -28,3 +28,22 @@ INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT(title) DESC;
+
+-- Deliverable 2: The employees eligible for the mentorship program 
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de 
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
